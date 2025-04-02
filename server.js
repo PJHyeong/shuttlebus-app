e1a9e201105995d5fb91a392afb0c756c3f297c6
PJHyeong <daeboodo14@naver.com>
2025-04-02 21:48:06 +0900

    메인 서버 파일 - 25/04/02 authRoutes추가

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
-// const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("./routes/authRoutes");
 // const busStopRoutes = require("./routes/busStopRoutes");
 // const announcementRoutes = require("./routes/announcementRoutes");
 
@@ -13,7 +13,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-// app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 // app.use("/api/bus", busStopRoutes);
 // app.use("/api/announcements", announcementRoutes);
 
