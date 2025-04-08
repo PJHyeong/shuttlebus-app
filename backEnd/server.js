8d7ad12f712081aadf3c7d05c2444020d0408a98
jcoding <kiwike00@naver.com>
2025-04-09 00:36:30 +0900

    메인 서버 파일 - 25/04/09 markersroute추가

--- backEnd/server.js
+++ backEnd/server.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 // const busStopRoutes = require("./routes/busStopRoutes");
 // const announcementRoutes = require("./routes/announcementRoutes");
+// const markersRoutes = require("./routes/markersRoutes");
 
 require("dotenv").config();
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 // app.use("/api/bus", busStopRoutes);
 // app.use("/api/announcements", announcementRoutes);
+// app.use('/api/markers', markersRoutes); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🟢 Server started on port ${PORT}`));
