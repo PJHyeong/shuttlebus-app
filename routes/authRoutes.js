5926705442b0d18c8c43401877fd44f12b1bd3fd
박재형 <152971192+PJHyeong@users.noreply.github.com>
2025-05-10 09:02:36 +0900

    Update authRoutes.js
    인증기능 (회원가입/로그인) - 25/05/10

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,12 +7,12 @@ const router = express.Router();
 //회원가입
 router.post("/register", async (req, res) => {
   try {
-      const { email, password, name } = req.body;
-      const existingUser = await User.findOne({ email });
+      const { studentid, password, name } = req.body;
+      const existingUser = await User.findOne({ studentid });
 
       if (existingUser) return res.status(400).json({ message: "이미 가입된 이메일입니다." });
 
-      const user = new User({ email, password, name });
+      const user = new User({ studentid, password, name });
       await user.save();
       
       res.status(201).json({ message: "회원가입 성공" });
@@ -23,17 +23,17 @@ router.post("/register", async (req, res) => {
 //로그인
 router.post("/login", async (req, res) => {
   try {
-      const { email, password } = req.body;
-      const user = await User.findOne({ email });
+      const { studentid, password } = req.body;
+      const user = await User.findOne({ studentid });
 
-      if (!user || !(await user.comparePassword(password))) {
+      if (!user || !(await user.comparePassword(password))) {   
           return res.status(400).json({ message: "이메일 또는 비밀번호가 잘못되었습니다." });
       }
 
       // JWT 토큰 생성
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" }); // 7일 동안 유효한 토큰 생성(유효기간 설정할 지 말지)
 
-      res.json({ token, user: { id: user._id, name: user.name, email: user.email } });
+      res.json({ token, user: { id: user._id, name: user.name, studentid: user.studentid } });
   } catch (error) {
       res.status(500).json({ message: "로그인 실패" });
   }
