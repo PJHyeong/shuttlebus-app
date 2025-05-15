dd9c60c0bb26c0e471acb9e4a113f0d92b8b112b
박재형 <152971192+PJHyeong@users.noreply.github.com>
2025-05-15 11:51:20 +0900

    delete

--- models/Notice.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const noticeSchema = new mongoose.Schema({
-    title: String,
-    content: String,
-    createdAt: { type: Date, default: Date.now}
-    });
-
-    module.exports = mongoose.model('Notice', noticeSchema); 
