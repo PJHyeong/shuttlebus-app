e1d868b339acd8531635dc73a34a03d95497804d
jcoding <kiwike00@naver.com>
2025-05-23 13:11:45 +0900

    협업을 위한 콜렉션 지정 삭제

--- backEnd/models/Buslocation.js
+++ backEnd/models/Buslocation.js
@@ -4,8 +4,6 @@ const busLocationSchema = new mongoose.Schema({
     latitude:  { type: Number, required: true },
     longitude: { type: Number, required: true },
     timestamp: { type: Date,   default: Date.now, index: true }
-  }, {
-    collection: 'gps'   // ← Atlas에 실제 데이터가 저장된 컬렉션 이름
   });
 
 module.exports = mongoose.model('BusLocation', busLocationSchema);
