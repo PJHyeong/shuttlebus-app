bb134ce447f0836ee9d024d2d5adf66b3f563006
jcoding <kiwike00@naver.com>
2025-05-23 13:12:52 +0900

    공지사항 조회 오류 해결

--- frontEnd/app/src/main/java/com/example/shuttlebusapplication/model/NoticeItem.kt
+++ frontEnd/app/src/main/java/com/example/shuttlebusapplication/model/NoticeItem.kt
@@ -6,8 +6,8 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class NoticeItem(
-    @SerializedName("_id")       val id: String,
-    val title:                    String,
-    @SerializedName("createdAt") val date: String,
-    val content:                  String
+    @SerializedName("_id")       val id: String = "",
+    val title:                    String = "",
+    @SerializedName("createdAt") val date: String = "",
+    val content:                  String = ""
 ) : Parcelable
