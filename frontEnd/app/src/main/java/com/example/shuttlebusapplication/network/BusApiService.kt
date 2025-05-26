97175be43ce209a6d33f40c8c4d1445fe11cc1a5
jcoding <kiwike00@naver.com>
2025-05-26 23:51:22 +0900

    api통합
    api통합

--- frontEnd/app/src/main/java/com/example/shuttlebusapplication/network/BusApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shuttlebusapplication.network
-
-import com.example.shuttlebusapplication.model.LocationResponse
-import retrofit2.http.GET
-
-interface BusApiService {
-    @GET("api/bus/latest")
-    suspend fun getLatestLocation(): LocationResponse
-}
