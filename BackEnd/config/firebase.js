9124be5bba3342505ff4661b3b43e8a99620df86
jcoding <kiwike00@naver.com>
2025-06-04 01:47:35 +0900

    초기화

--- BackEnd/config/firebase.js
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)   // Firebase Admin SDK 초기화
-});
-
-module.exports = admin; 
--- BackEnd/models/Buslocation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const busLocationSchema = new mongoose.Schema({
-    latitude:  { type: Number, required: true },
-    longitude: { type: Number, required: true },
-    timestamp: { type: Date,   default: Date.now, index: true }
-  });
-
-module.exports = mongoose.model('BusLocation', busLocationSchema);
