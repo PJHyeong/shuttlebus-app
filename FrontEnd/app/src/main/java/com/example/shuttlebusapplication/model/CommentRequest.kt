68aff7d87751a9cb9769bc35c10f339f35dfee84
taeu <taeu5597@naver.com>
2025-06-06 12:05:55 +0900

    🎯 design 브랜치 초기화 및 프론트+백 통합

--- FrontEnd/app/src/main/java/com/example/shuttlebusapplication/model/CommentRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.example.shuttlebusapplication.model
-
-data class CommentRequest(
-    val announcementId: String,
-    val userId: String,  // 현재 사용자의 ID
-    val content: String
-)
--- FrontEnd/app/src/main/java/com/example/shuttlebusapplication/model/CommentResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.shuttlebusapplication.model
-
-data class CommentResponse(
-    val id: String,
-    val userId: String,
-    val content: String,
-    val createdAt: String
-)
